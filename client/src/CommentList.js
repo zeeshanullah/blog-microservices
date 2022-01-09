8646b0d681142f2aa18c27a817ed7dc2b3ad1c02
Zeeshan Ullah <zeeshan.ullah@doccheck.com>
2022-01-09 18:53:22 +0100

    updated client to show comment based on status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,8 +2,22 @@ import React from "react";
 
 const CommentList = ({ comments }) => {
     const renderedComments = comments.map(comment => {
+        let content;
+
+        switch (comment.status) {
+            case 'rejected':
+                content = 'This comment has been rejected'; 
+                break;
+            case 'pending':
+                content = 'This comment is awaiting approval';
+                break;
+            default:
+                content = comment.content; 
+                break;
+        }
+
         return <div key={comment.id}>
-            <li>{comment.content}</li>
+            <li>{content}</li>
         </div>
     });
     return <ul>
