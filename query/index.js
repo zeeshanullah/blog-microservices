0784519bc3dba2779f804afba281df9a691a1348
Zeeshan Ullah <zeeshan.ullah@doccheck.com>
2022-01-09 20:58:49 +0100

    fetch and process events on start

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const {type, data} = req.body;
-
+const handleEvents = (type, data) => {
     if (type === 'PostCreated') {
         const {id, title} = data;
 
@@ -39,10 +34,27 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const {type, data} = req.body;
+
+    handleEvents(type, data);
 
     res.send({});
 });
 
-app.listen(4002, () => {
-    console.log('Listening to 4200');
+app.listen(4002, async () => {
+    console.log('Listening to 4002');
+
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+        console.log('Processing event: ', event.type);
+        handleEvents(event.type, event.data);
+    }
 });
